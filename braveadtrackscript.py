dc52a652ae2d21f17962014d3b9ba39f2c5f96e2
Af7eR9l0W <85209771+Af7eR9l0W@users.noreply.github.com>
2022-01-09 09:08:48 -0600

    Script updated and runs faster

--- braveadtrackscript.py
+++ braveadtrackscript.py
@@ -1,9 +1,9 @@
-#need pyautogui keyboard isnt used
+#lol thx copilot for helping me make this in literally 30 seconds
 import pyautogui
-import keyboard 
 import time
+import keyboard
 
-print('Open 5 tabs in a chrome based browser')
+print('Open up to 10 tabs in a chrome based browser')
 time.sleep(.2)
 print('Go to cnn.com or a site with a lot of trackers')
 print('spam q to quit or ctrl+c')
@@ -25,6 +25,16 @@ while (True):
     pyautogui.press('f5') #refreshes page
     pyautogui.hotkey('ctrl','5')  #switches to fifth tab
     pyautogui.press('f5') #refreshes page
+    pyautogui.hotkey('ctrl','6')  #switches to sixth tab
+    pyautogui.press('f5') #refreshes page
+    pyautogui.hotkey('ctrl','7')  #switches to seventh tab
+    pyautogui.press('f5') #refreshes page
+    pyautogui.hotkey('ctrl','8')  #switches to eighth tab
+    pyautogui.press('f5') #refreshes page
+    pyautogui.hotkey('ctrl','9')  #switches to ninth tab
+    pyautogui.press('f5') #refreshes page
+    pyautogui.hotkey('ctrl','0')  #switches to tenth tab
+    pyautogui.press('f5') #refreshes pag
     count = count + 1
     pyautogui.sleep(.5)
     
@@ -35,7 +45,9 @@ while (True):
             break
 	 
     
+count = count*10    
 print('You refreshed about', count, 'times')
+print('in total you blocked around', count*14, 'trackers')
     
     
     
